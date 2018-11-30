2a4de524c24bb56d1b1e3afe3ac265bafa7522d7
Mark Pevec <mark.pevec@axonvct.com>
2018-11-30 14:34:37 -0500

    test

--- container1/index.js
+++ container1/index.js
@@ -2,7 +2,7 @@
 const request = require('request')
 
 let startTime = new Date()
-request({ url: 'http://localhost:8081/hello', method: 'GET' }, (err, res, body) => {
+request({ url: 'http://step_1:8081/hello', method: 'GET' }, (err, res, body) => {
   console.log('Received error: ' + err)
   console.log('Received response: ' + res)
   console.log('Received, body: ' + body)  
