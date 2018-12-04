0f77f9b6949d6d3a3eabbc5bcc88315202c52776
Mark Pevec <mark.pevec@axonvct.com>
2018-12-04 11:07:45 -0500

    test

--- container1/index.js
+++ container1/index.js
@@ -2,13 +2,11 @@
 const { Client } = require('pg')
 
 setTimeout(() => {
-  console.log(process.env.PGHOST)
-  console.log(process.env.PGUSER)
   const client = new Client()
   client.connect().then(res => {
     console.log('Connected!')
   }, err => {
     console.error('Could not connect, error: ' + JSON.stringify(err))
   })
-}, 40000)
+}, 25000)
 
